fdfbf49804da90f80db6d1415d10c6cc764761f7
JuneTech <junetech0@gmail.com>
2020-05-29 01:39:45 +0900

    added MasterMetadata

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 """main for FCCA MIP model solving
 """
+import json
 import datetime
 from time import time
 
@@ -7,21 +8,37 @@ from params_fcca import ParamsFCCA
 from fcca_mip import make_fcca_mip_model
 
 
+class MasterMetadata:
+    """Data of data files
+    """
+    env_json_filename: str
+    veh_file_postfix: str
+    veh_file_ext: str
+    json_encoding: str
+    lp_filename: str
+
+    def __init__(self, filename: str):
+        with open(filename) as f_data:
+            _dict = json.load(f_data)
+            for key, value in _dict.items():
+                self.__dict__[key] = value
+
+
 def main():
     """read and solve
     """
-    env_json_filename = "params_env.json"
-    veh_file_postfix = "params_veh_"
-    veh_file_ext = ".json"
-    encoding = "utf-8"
+    master_metadata_filename = "master_metadata.json"
+    metadata = MasterMetadata(master_metadata_filename)
 
-    params_fcca = ParamsFCCA(env_json_filename,
-                             veh_file_postfix,
-                             veh_file_ext,
-                             encoding)
+    params_fcca = ParamsFCCA(metadata.env_json_filename,
+                             metadata.veh_file_postfix,
+                             metadata.veh_file_ext,
+                             metadata.json_encoding)
 
     fcca_model = make_fcca_mip_model(params_fcca)
 
+    # write to .lp file
+    fcca_model.write(metadata.lp_filename)
     fcca_model.optimize()
 
 
